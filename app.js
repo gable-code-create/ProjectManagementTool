85c180db17bc9de5596307a18fca2d7b84e00f7e
gable-code-create <61628091+gable-code-create@users.noreply.github.com>
2021-07-31 00:28:15 -0400

    Update app.js

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ const app = express();
 const path = require('path');
 
 const port = process.env.PORT || 3000;
-
-
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+})
 
 app.get('/api', (req, res) => {
     res.setHeader('Content-Type', 'application/json');
