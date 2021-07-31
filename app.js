d0e54299c1a02fdbb61a0b794370cf4d29549e03
gable-code-create <61628091+gable-code-create@users.noreply.github.com>
2021-07-30 21:21:21 -0400

    Update app.js

--- app.js
+++ app.js
@@ -7,12 +7,10 @@ const path = require('path');
 const port = process.env.PORT || 5000;
 
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static('build'));
-    app.get('*', (req, res) => {
-        req.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-    })
-}
+app.get('/', function (req, res){
+    res.send('hello word');
+})
+
 app.listen(port, (err) => {
     if(err) return console.log(err);
     console.log('Server running at ', port);
