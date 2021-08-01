09ea9785f7ab1536c462ecd0c609d020b60d8eb2
gable-code-create <61628091+gable-code-create@users.noreply.github.com>
2021-08-01 15:51:28 -0400

    update aug1

--- app.js
+++ app.js
@@ -1,11 +1,54 @@
+const AWS = require('aws-sdk');
+require('dotenv').config(); 
+
 const express = require('express');
 
 const app = express();
 
 const path = require('path');
+const bodyParser = require('body-parser');
+const pino = require('express-pino-logger')();
 
 const port = process.env.PORT || 3000;
+
+
+
+
+AWS.config.update({
+    region: process.env.AWS_DEFAULT_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY
+  });
+  
+  //needs to be after config
+  const dynamoClient = new AWS.DynamoDB.DocumentClient();
+  const TABLE_NAME = 'type';
+  
+
 app.use(express.static(path.join(__dirname, 'build')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(pino);
+
+var params = {
+    TableName: TABLE_NAME,
+    Key:{"id":0}
+  }
+  
+  const getData = async () => {
+    const params = {
+      TableName: TABLE_NAME,
+      Key:{"id":0}
+    }
+  
+    const userT = await dynamoClient.scan(params).promise();
+    return userT;
+  }
+
+
+app.listen(port, (err) => {
+    if(err) return console.log(err);
+    console.log('Server running at ', port);
+})
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
@@ -15,7 +58,16 @@ app.get('/api', (req, res) => {
     res.send({alldata: `Your username is`} || 'wow');
 })
 
-app.listen(port, (err) => {
-    if(err) return console.log(err);
-    console.log('Server running at ', port);
+app.get('/this', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+     
+ dynamoClient.get(params, function (err, data) {
+  
+
+
+    console.log(data.Items[0].name);
+     
+      res.send({alldata: `this is the data ${data.Items[0].name}`});
+      
+    })
 })
